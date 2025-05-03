eb8da8e2c70dc6245442b30a38f10310d51a5afa
sans2470 <agrawalsanskriti9@gmail.com>
2025-05-03 14:14:56 +0000

    Python basics - variables and print

--- basics.py
+++ basics.py
@@ -1,6 +1,27 @@
-student={"name":"lisa"}
-print(student)
-student={"name":"Lisa","maths":"90","DOB":"1-08-2004","hobbies":"painting"}
-print(student)
-print(student["DOB"])
+print("part 1: basic variables -- store and access data")
 
+x = 10
+y = 20
+z = x+y
+
+print("this is the solution=",z)
+print("solution=",z)
+
+print("x+y")
+
+print("x+y=",z)
+print(x+y)
+
+# Evaluate expression inside the double quotes ("")
+print("the value of x+y is {x+y}")
+
+print(f"the value of x+y is {x+y}")
+
+
+print("\n\npart 1.1: basic variables -- strings")
+
+a="lisa"
+
+print(a)
+A="sans"
+print(A)
