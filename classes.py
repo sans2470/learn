7d3db9b346090570608661327c79eb67236e249d
sans2470 <agrawalsanskriti9@gmail.com>
2025-05-09 15:10:25 +0000

    classes

--- classes.py
+++ classes.py
@@ -1,15 +1,22 @@
 
 class Dog:
 
-    def __init__(self, name):
+    def __init__(self, name, dob): # attributes
         self.name = name
+        self.dob = dob
 
     def talk(self): # def f(x):
         print(f"{self.name} says woof!")
+    
+    def tellmeDOB(self):
+        print(f"{self.name} birthday is {self.dob}")
 
 # timeline
-jojo = Dog("jojo") # jojo is born
-chocolate = Dog("chocolate") # chocolate is born
+jojo = Dog("jojo", "2022-01-01") # jojo is born
+chocolate = Dog("chocolate", "2023-02-02") # chocolate is born
 
 jojo.talk() 
 chocolate.talk()
+
+jojo.tellmeDOB()
+chocolate.tellmeDOB()
